8f6bf0a8edb61fe51feb51b311d2f187820acc79
RhuanYuri <rhuanyuri12@gmail.com>
2025-07-12 11:30:04 -0300

    fix: Ajustar lógica para cadastrar o status do pedido

--- src/view/janela1.py
+++ src/view/janela1.py
@@ -77,10 +77,13 @@ class Janela1:
                 status_aux = int(input('status: 1-preparo, 2-pronto, 3-entregue: '))
                 if status_aux == 1:
                     status = 'preparo'
-                if status_aux == 2:
+                elif status_aux == 2:
                     status = 'pronto'
-                else:
+                elif status_aux == 3:
                     status = 'entregue'
+                else:
+                    print('Valor incorreto, recomeçando')
+                    break
  
                 print(f'Valor Final: R${valor_total}')
                 data_hoje = date.today()
